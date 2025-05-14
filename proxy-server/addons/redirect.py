aba5ce4d699e4e21859138139de11b8fb287bc51
Zion Huang <zionhuang1107@gmail.com>
2025-05-14 09:51:58 +0800

    fix: redirect for https traffic

--- proxy-server/addons/redirect.py
+++ proxy-server/addons/redirect.py
@@ -4,17 +4,27 @@ DOMAIN = os.getenv('DOMAIN')
 
 
 class RedirectManager:
-    def request(self, flow: http.HTTPFlow):
+    def redirect(self, flow: http.HTTPFlow):
         hostname = flow.request.pretty_host
         if hostname == DOMAIN:
             flow.request.host = 'frontend'
             flow.request.port = 80
+            flow.request.scheme = "http"
         elif hostname == f'backend.{DOMAIN}':
             flow.request.host = 'backend'
             flow.request.port = 8000
+            flow.request.scheme = "http"
         elif hostname == f'grafana.{DOMAIN}':
             flow.request.host = 'grafana'
             flow.request.port = 3000
+            flow.request.scheme = "http"
         elif hostname == f'influxdb.{DOMAIN}':
             flow.request.host = 'influxdb'
             flow.request.port = 8086
+            flow.request.scheme = "http"
+
+    def request(self, flow: http.HTTPFlow):
+        self.redirect(flow)
+
+    def http_connect(self, flow: http.HTTPFlow):
+        self.redirect(flow)
